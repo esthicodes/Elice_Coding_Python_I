761d214a9bcdd98609d38366a9e6511871f90538
Jacqueline Esther Hoeun Yu <togom11@g.skku.edu>
2021-11-09 16:41:10 +0900

    Create Proposition.py
    Making a proposition.
    Use the comparison operator we learned earlier to create a proposition that satisfies the given conditions and put it in! A proposition is a sentence or expression that can determine whether the content is true or false.
    
    Let's put a proposition that satisfies the conditions given to the three variables on the right!

---
+++ Proposition.py
@@ -0,0 +1,26 @@
+# # # "3 is not equal to 5". 
+# Save the above proposition in ans1 using the # # == or != operator.
+# # ans1 = None
+
+# # # "21 is a number greater than 5*10."
+# Save the above proposition in ans2 using # # # or < operator.
+# # ans2 = None
+
+# # # "5/3 share is greater than or equal to 1".
+# Save the above proposition in ans3 using the # #>= or <= operator.
+# # ans3 = None
+
+# # Check the true and false by outputting the above three variables. You don't have to modify the code below.
+# print(ans1, ans2, ans3)
+
+# Let's put the proposition True into ans1 using the # Q1. = or != operator.
+ans1 = 1!=0
+
+# Let's put the False proposition into ans2 using # Q2. > or < operator.
+ans2 = 2<1
+
+# Let's put the proposition True into ans3 using the # Q3. >= or <= operator.
+ans3 = 2<=3
+
+# Let's print out the above three variables and check whether they are True or False.
+print(ans1, ans2, ans3)
