86c408abb8b7ffb265a697a5909a963b9d992725
Jacqueline Esther Hoeun Yu <togom11@g.skku.edu>
2021-11-09 16:20:12 +0900

    Create copycat parrot.py

---
+++ copycat parrot.py
@@ -0,0 +1,21 @@
+# Copycat parrot.
+# The input is to deliver information to the computer. When we give 'additional information' to the computer through input, the computer takes it and executes the command.
+
+# Let's use input and print to create a parrot program that outputs the input as it is!
+
+
+# Let's do it like this!
+# Use the input() function to write the code that receives the entered value. Please save the entered value in the variable var.
+# Press the run button.
+# Please put the terminal input value under the execution result window. Feel free to write down what you want to say in the section and press Enter.
+# What does a parrot say?
+# Press the Submit button to check the score.
+
+# Let's put in the input using #input() and put it in the variable var.
+var=input()
+
+# Parrot copies what he says!
+print ("Parrot :", var)
+
+# Let's check what data type the value you entered is.
+print(type(var))
