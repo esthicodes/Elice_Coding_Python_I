1b2769b57b34be935df1e778e25e252ca5e64526
Jacqueline Esther Hoeun Yu <togom11@g.skku.edu>
2021-11-09 16:19:14 +0900

    Create Flamingo Game.py
    Let's get the input in #variable money.
    
    money=input()
    Let's convert # money into int type and put it back into money.
    money=int(money)
    
    Let's double the # money and print it out.
    money = money*2
    print(money)

---
+++ Flamingo Game.py
@@ -0,0 +1,28 @@
+# Flamingo that gives twice as much.
+# Flamingo, a banker at Coderland E Bank, works twice as hard as others. If you leave the money to a flamingo who works twice as hard, it will return exactly twice the amount a year later. Wow!
+
+# Let's write a code that outputs the money you can receive a year later when you enter the amount to leave to flamingo.
+
+
+# Let's do it like this!
+# After declaring the variable money, write the code to receive user input.
+# Please convert the input value to an integer.
+# Write a formula so that the amount is doubled.
+# Print out money.
+# Press the run button to check the results.
+# Press the Submit button to check the score.
+# Tips
+# Input() recognizes the input value as a string, so you have to transform it!
+
+# Change your name to an integer. 
+
+
+# Let's get the input in #variable money.
+
+# money=input()
+# Let's convert # money into int type and put it back into money.
+money=int(money)
+
+# Let's double the # money and print it out.
+money = money*2
+print(money)
